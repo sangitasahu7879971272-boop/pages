630f9721440a9dbd25488db9731d05478ef02003
Sangita Sahu <sangitasahu7879971272@gmail.com>
2025-08-13 16:57:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from '@/components/layout/header';
 import { Hero } from '@/components/landing/hero';
-import { Footer } from '@/components/layout/footer';
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
       </main>
-      <Footer />
     </div>
   );
 }
